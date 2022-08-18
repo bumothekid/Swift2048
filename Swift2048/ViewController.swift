99c731469f708803bc39a1bb81d1f92bef397e43
David Riegel <40246197+bumothekid@users.noreply.github.com>
2022-08-18 22:56:59 +0200

    Initial Commit

---
+++ Swift2048/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift2048
+//
+//  Created by David Riegel on 18.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
