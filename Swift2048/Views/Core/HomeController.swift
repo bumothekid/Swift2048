15dda5434dbd3e2b1dc6c53e2827663023d179a4
David Riegel <40246197+bumothekid@users.noreply.github.com>
2022-08-19 15:06:43 +0200

    Configure Home Controller with placeholder Label for the logo and a start Button

--- Swift2048/Views/Core/HomeController.swift
+++ Swift2048/Views/Core/HomeController.swift
@@ -11,9 +11,39 @@ class HomeController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureViewComponents()
     }
+    
+    lazy var logoPlaceholderLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Swift 2048"
+        label.textColor = .label
+        label.font = .systemFont(ofSize: 38, weight: .bold)
+//        label.backgroundColor = UIColor(named: "2048 Color")
+//        label.layer.cornerRadius = 10
+        return label
+    }()
+    
+    lazy var startButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Start Game", for: .normal)
+        button.setTitleColor(UIColor.label, for: .normal)
+        button.layer.cornerRadius = 5
+        button.backgroundColor = .systemCyan
+        return button
+    }()
 
-
+    func configureViewComponents() {
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(logoPlaceholderLabel)
+        logoPlaceholderLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        logoPlaceholderLabel.anchor(top: view.safeAreaLayoutGuide.centerYAnchor, paddingTop: -75)
+        
+        view.addSubview(startButton)
+        startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        startButton.anchor(top: view.safeAreaLayoutGuide.centerYAnchor, paddingTop: 0, width: 150)
+    }
 }
 
